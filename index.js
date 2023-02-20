553c498a9a1f0bb131794a8946f2adcb70f49361
everitosan <everardo.ipn@gmail.com>
2023-02-19 22:03:34 -0600

    fix: default template path

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const ffi = require("ffi-napi")
 const path = require('path')
-
+import * as pack from "./package.json"
 
 const library_name = path.resolve(__dirname, './blueprint/target/release/libblueprintlib');
 
@@ -8,11 +8,9 @@ const lib = ffi.Library(library_name, {
   blueprint: ["bool", ["string", "string", "string"]]
 })
 
-
-function entry(source, destination, template = "blueprint/templates/Blueprint.svelte") {
+function entry(source, destination, template = `${pack.name}/templates/Blueprint.svelte`) {
   return lib.blueprint(source, destination, template)
 }
-
 
 module.exports = {
   document: entry
